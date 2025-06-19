f33a4e1d35338d44c5e66a45b45114c77e5edb41
Luqman Rasyid <luqmanrasyid45@gmail.com>
2025-06-19 22:34:43 +0700

    test

--- src/app/destinasi/page.jsx
+++ src/app/destinasi/page.jsx
@@ -17,7 +17,7 @@ export default function Home() {
               href="/"
               className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Go back home
+              Go back homee
             </Link>
           </div>
         </div>
